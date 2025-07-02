dc85a5832bccc94412265625f1244405aa85699c
Erik Simon <10850738+ShooTeX@users.noreply.github.com>
2025-07-02 19:45:34 +0200

    add flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "listy dev tools";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.cobra-cli
+          ];
+
+          shellHook = ''
+            echo "Dev shell ready for ${system}"
+          '';
+        };
+      }
+    );
+}
